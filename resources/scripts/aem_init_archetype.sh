e9f8febd019ba74f3928e1b389100e960a86a45c
Herman Ciechanowiec <herman@ciechanowiec.eu>
2022-09-13 17:12:59 +0200

    Add snapshot version

--- resources/scripts/aem_init_archetype.sh
+++ resources/scripts/aem_init_archetype.sh
@@ -6,6 +6,7 @@
 # 2. As of creating this script, if for project generation Java 11+ is used, it will result
 #    in unformatted `/pom.xml` (see bug description: https://issues.apache.org/jira/browse/ARCHETYPE-587).
 #    In order to avoid it, Java 8 is used.
+# 3. Use -SNAPSHOT version so that the bundle will be overwritten every time it is reinstalled (details: https://sling.apache.org/documentation/bundles/osgi-installer.html#:~:text=The%20OSGi%20installer%20is%20a,for%20the%20OSGi%20configuration%20admin)
 
 source "$HOME/.sdkman/bin/sdkman-init.sh" # To make sdk command work
 
@@ -21,7 +22,7 @@ mvn -B org.apache.maven.plugins:maven-archetype-plugin:3.2.1:generate \
     -D artifactId="firsthops" \
     -D package="eu.ciechanowiec.firsthops" \
     -D includeExamples=y \
-    -D version="1.0.0" \
+    -D version="1.0-SNAPSHOT" \
     -D aemVersion="6.5.13"
 
 sdk use java 11.0.16-tem
