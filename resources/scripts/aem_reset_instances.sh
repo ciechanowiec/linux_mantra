aa84cae29a02a73a6cd495e943183bccf5accc39
Herman Ciechanowiec <herman@ciechanowiec.eu>
2022-10-06 19:18:39 +0200

    Introduce AEM Forms

--- resources/scripts/aem_reset_instances.sh
+++ resources/scripts/aem_reset_instances.sh
@@ -3,10 +3,13 @@
 # Deployment folders structure
 #  aem-sdk
 #  ├───author
-#  │   ├───aem-author-p4502.jar
+#  │   ├───aem-quickstart-p4502.jar
+#  │   └───license.properties
+#  ├───forms
+#  │   ├───aem-quickstart-p4504.jar
 #  │   └───license.properties
 #  ├───publish
-#  │   ├───aem-publish-p4503.jar
+#  │   ├───aem-quickstart-p4503.jar
 #  │   └───license.properties
 #  ├───adobe-aemfd-linux-pkg-6.0.334.zip
 #  ├───aem-service-pkg-6.5.14.0.zip
@@ -14,6 +17,9 @@
 #  ├───formsContent.zip
 #  └───testContent.zip
 
+# ============================================== #
+#                   VARIABLES                    #
+# ============================================== #
 echo "Setting up variables..."
 # Current directory
 currentDir=$(pwd)
@@ -25,31 +31,47 @@ servicePackage="$rootDir/aem-service-pkg-6.5.14.0.zip"
 basicContent="$rootDir/basicContent.zip"
 formsContent="$rootDir/formsContent.zip"
 testContent="$rootDir/testContent.zip"
-# Author/Publish directories
+# Instance directories
 authorDir="$rootDir/author"
+formsDir="$rootDir/forms"
 publishDir="$rootDir/publish"
 # AEM JARs
-authorJAR="$authorDir/aem-author-p4502.jar"
-publishJAR="$publishDir/aem-publish-p4503.jar"
+authorJAR="$authorDir/aem-quickstart-p4502.jar"
+formsJAR="$formsDir/aem-quickstart-p4504.jar"
+publishJAR="$publishDir/aem-quickstart-p4503.jar"
+# Licenses
+authorLicense="$authorDir/license.properties"
+formsLicense="$formsDir/license.properties"
+publishLicense="$publishDir/license.properties"
 # JCR paths
 authorJCR="$authorDir/crx-quickstart"
+formsJCR="$formsDir/crx-quickstart"
 publishJCR="$publishDir/crx-quickstart"
-# sling.properties on AEM Author
-slingPropsOfAuthor="$authorDir/crx-quickstart/conf/sling.properties"
+# sling.properties on AEM Author and AEM Forms
+authorSlingProps="$authorDir/crx-quickstart/conf/sling.properties"
+formsSlingProps="$formsDir/crx-quickstart/conf/sling.properties"
 # Initial installation directories
 authorInstallDir="$authorJCR/install"
+formsInstallDir="$formsJCR/install"
 publishInstallDir="$publishJCR/install"
 
+# ============================================== #
+#                     CHECKS                     #
+# ============================================== #
 echo "Checking whether required directories and files exist..."
-if [ ! -d "$rootDir" ] || [ ! -f "$formsPackage" ] || [ ! -f "$servicePackage" ] \
-   || [ ! -f "$basicContent" ] || [ ! -f "$formsContent" ] || [ ! -f "$testContent" ] \
-   || [ ! -d "$authorDir" ] || [ ! -d "$publishDir" ] \
-   || [ ! -f "$authorJAR" ] || [ ! -f "$publishJAR" ]
+if [ ! -d "$rootDir" ] \
+   || [ ! -f "$formsPackage" ] || [ ! -f "$servicePackage" ] || [ ! -f "$basicContent" ] || [ ! -f "$formsContent" ] || [ ! -f "$testContent" ] \
+   || [ ! -d "$authorDir" ] || [ ! -d "$formsDir" ] || [ ! -d "$publishDir" ] \
+   || [ ! -f "$authorJAR" ] || [ ! -f "$formsJAR" ] || [ ! -f "$publishJAR" ] \
+   || [ ! -f "$authorLicense" ] || [ ! -f "$formsLicense" ]  || [ ! -f "$publishLicense" ]
  then
    echo "At least one of required directories or files doesn't exist. Aborting..."
    exit 1
 fi
 
+# ============================================== #
+#                   AEM AUTHOR                   #
+# ============================================== #
 echo "Resetting AEM Author instance..."
 cd "$authorDir" || exit 1
 if [ -d "$authorJCR" ]
@@ -66,16 +88,44 @@ cp "$testContent" "$authorInstallDir"
 cd "$currentDir" || exit 1
 
 echo "Setting up AEM Forms bootstrap properties..."
-# 1. Two following lines should be added to $slingPropsOfAuthor for AEM Forms to work:
-#      sling.bootdelegation.class.org.bouncycastle.jce.provider.BouncyCastleProvider=org.bouncycastle.*
-#      sling.bootdelegation.class.com.rsa.jsafe.provider.JsafeJCE=com.rsa.*
-# 2. Be aware that in order for 'AEMFD Signatures Bundle (adobe-aemfd-signatures)'
-#    from AEM Forms to work, AEM should be restarted after the first start.
-cat >> "$slingPropsOfAuthor" << EOF
+# Two following lines should be added to $authorSlingProps for AEM Forms to work:
+#   sling.bootdelegation.class.org.bouncycastle.jce.provider.BouncyCastleProvider=org.bouncycastle.*
+#   sling.bootdelegation.class.com.rsa.jsafe.provider.JsafeJCE=com.rsa.*
+cat >> "$authorSlingProps" << EOF
 sling.bootdelegation.class.org.bouncycastle.jce.provider.BouncyCastleProvider=org.bouncycastle.*
 sling.bootdelegation.class.com.rsa.jsafe.provider.JsafeJCE=com.rsa.*
 EOF
 
+# ============================================== #
+#                    AEM FORMS                   #
+# ============================================== #
+echo "Resetting AEM Forms instance..."
+cd "$formsDir" || exit 1
+if [ -d "$formsJCR" ]
+  then
+    rm -rf "$formsJCR"
+fi
+java -jar "$formsJAR" -unpack
+mkdir "$formsInstallDir"
+cp "$servicePackage" "$formsInstallDir"
+cp "$formsPackage" "$formsInstallDir"
+cp "$basicContent" "$formsInstallDir"
+cp "$formsContent" "$formsInstallDir"
+cp "$testContent" "$formsInstallDir"
+cd "$currentDir" || exit 1
+
+echo "Setting up AEM Forms bootstrap properties..."
+# Two following lines should be added to $formsSlingProps for AEM Forms to work:
+#   sling.bootdelegation.class.org.bouncycastle.jce.provider.BouncyCastleProvider=org.bouncycastle.*
+#   sling.bootdelegation.class.com.rsa.jsafe.provider.JsafeJCE=com.rsa.*
+cat >> "$formsSlingProps" << EOF
+sling.bootdelegation.class.org.bouncycastle.jce.provider.BouncyCastleProvider=org.bouncycastle.*
+sling.bootdelegation.class.com.rsa.jsafe.provider.JsafeJCE=com.rsa.*
+EOF
+
+# ============================================== #
+#                  AEM PUBLISH                   #
+# ============================================== #
 echo "Resetting AEM Publish instance..."
 # In this particular case $formsPackage and $formsContent aren't supposed to be installed on AEM Publish
 cd "$publishDir" || exit 1
