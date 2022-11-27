025cb784eb9cded7dda621456ef3e654ba9e4802
Herman Ciechanowiec <herman@ciechanowiec.eu>
2022-11-27 10:55:38 +0100

    Update AEM archetype initialization procedure

--- resources/scripts/aem_init_archetype.sh
+++ resources/scripts/aem_init_archetype.sh
@@ -6,9 +6,18 @@
 # 2. As of creating this script, if for project generation Java 11+ is used, it will result
 #    in unformatted `/pom.xml` (see bug description: https://issues.apache.org/jira/browse/ARCHETYPE-587).
 #    In order to avoid it, Java 8 is used.
-# 3. Use -SNAPSHOT version so that the bundle will be overwritten every time it is reinstalled (details: https://sling.apache.org/documentation/bundles/osgi-installer.html#:~:text=The%20OSGi%20installer%20is%20a,for%20the%20OSGi%20configuration%20admin)
+# 3. Use -SNAPSHOT version so that the bundle will be overwritten every time it is reinstalled
+#    (details: https://sling.apache.org/documentation/bundles/osgi-installer.html#:~:text=The%20OSGi%20installer%20is%20a,for%20the%20OSGi%20configuration%20admin)
 
 source "$HOME/.sdkman/bin/sdkman-init.sh" # To make sdk command work
+
+currentDir=$(pwd)
+appId="firsthops"
+targetDir="${currentDir}/${appId}"
+corePom="$targetDir/core/pom.xml"
+allPom="$targetDir/all/pom.xml"
+parentPom="$targetDir/pom.xml"
+latestConditionalLibVersion=$(curl --silent https://repo.maven.apache.org/maven2/eu/ciechanowiec/conditional/maven-metadata.xml | grep '<latest>' | cut -d '>' -f 2 | cut -d '<' -f 1)
 
 sdk use java 8.0.345-tem
 
@@ -17,12 +26,56 @@ mvn -B org.apache.maven.plugins:maven-archetype-plugin:3.2.1:generate \
     -D archetypeArtifactId=aem-project-archetype \
     -D archetypeVersion=39 \
     -D appTitle="First Hops" \
-    -D appId="firsthops" \
+    -D appId="$appId" \
     -D groupId="eu.ciechanowiec" \
-    -D artifactId="firsthops" \
-    -D package="eu.ciechanowiec.firsthops" \
+    -D artifactId="$appId" \
+    -D package="eu.ciechanowiec.$appId" \
+    -D singleCountry=n \
     -D includeExamples=y \
     -D version="1.0-SNAPSHOT" \
     -D aemVersion="6.5.13"
 
 sdk use java 11.0.16-tem
+
+cd "$targetDir" || exit 1
+git init
+git add .
+git commit -m "Init commit"
+
+sed -i 's/<source>1.8<\/source>/<source>11<\/source>/g' "$parentPom"
+sed -i 's/<target>1.8<\/target>/<target>11<\/target>/g' "$parentPom"
+
+allPomFirstPart=$(head -n 56 "$allPom")
+allPomSecondPart=$(tail -n +57 "$allPom")
+echo "$allPomFirstPart" > "$allPom"
+cat >> "$allPom" << EOF
+                        <embedded>
+                            <groupId>eu.ciechanowiec</groupId>
+                            <artifactId>conditional</artifactId>
+                            <target>/apps/firsthops-vendor-packages/application/install</target>
+                        </embedded>
+EOF
+echo "$allPomSecondPart" >> "$allPom"
+
+corePomFirstPart=$(head -n 83 "$corePom")
+corePomSecondPart=$(tail -n +84 "$corePom")
+echo "$corePomFirstPart" > "$corePom"
+cat >> "$corePom" << EOF
+        <dependency>
+            <groupId>org.projectlombok</groupId>
+            <artifactId>lombok</artifactId>
+            <version>1.18.24</version>
+            <scope>provided</scope>
+        </dependency>
+        <dependency>
+            <groupId>eu.ciechanowiec</groupId>
+            <artifactId>conditional</artifactId>
+            <version>$latestConditionalLibVersion</version>
+        </dependency>
+EOF
+echo "$corePomSecondPart" >> "$corePom"
+
+git add .
+git commit -m "Add libraries, migrate to Java 11"
+
+cd "$currentDir" || exit 1
