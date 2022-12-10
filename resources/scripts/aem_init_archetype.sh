143cb533cb1c5c69087bb3da85f441293faa38b4
Herman Ciechanowiec <herman@ciechanowiec.eu>
2022-12-10 13:52:03 +0100

    Specify POM location

--- resources/scripts/aem_init_archetype.sh
+++ resources/scripts/aem_init_archetype.sh
@@ -4,7 +4,7 @@
 #    However, as of creating this script (2022-08-27), no `uber-jar` of the
 #    version 6.5.14 was available. For that reason aemVersion was set to 6.5.13.
 # 2. As of creating this script, if for project generation Java 11+ is used, it will result
-#    in unformatted `/pom.xml` (see bug description: https://issues.apache.org/jira/browse/ARCHETYPE-587).
+#    in unformatted `{basedir}/pom.xml` (see bug description: https://issues.apache.org/jira/browse/ARCHETYPE-587).
 #    In order to avoid it, Java 8 is used.
 # 3. Use -SNAPSHOT version so that the bundle will be overwritten every time it is reinstalled
 #    (details: https://sling.apache.org/documentation/bundles/osgi-installer.html#:~:text=The%20OSGi%20installer%20is%20a,for%20the%20OSGi%20configuration%20admin)
