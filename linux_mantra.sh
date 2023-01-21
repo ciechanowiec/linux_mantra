e915873b1cecc004cef778fb13059553e074a7ed
Herman Ciechanowiec <herman@ciechanowiec.eu>
2023-01-21 09:39:02 +0100

    Change update config

--- linux_mantra.sh
+++ linux_mantra.sh
@@ -743,7 +743,7 @@ sudo bash -c "cat > ${aptUpdateConfigFile} << EOF
 APT::Periodic::Update-Package-Lists "0";
 APT::Periodic::Download-Upgradeable-Packages "0";
 APT::Periodic::AutocleanInterval "0";
-APT::Periodic::Unattended-Upgrade "1";
+APT::Periodic::Unattended-Upgrade "0";
 EOF
 "
 
