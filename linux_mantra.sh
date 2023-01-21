9c0a648049d14e54edc65f4cf445ae9b7e573472
Herman Ciechanowiec <herman@ciechanowiec.eu>
2023-01-22 00:18:27 +0100

    Escape double quotes

--- linux_mantra.sh
+++ linux_mantra.sh
@@ -740,10 +740,10 @@ informAboutProcedureStart
 aptUpdateConfigFile="/etc/apt/apt.conf.d/20auto-upgrades"
 
 sudo bash -c "cat > ${aptUpdateConfigFile} << EOF
-APT::Periodic::Update-Package-Lists "0";
-APT::Periodic::Download-Upgradeable-Packages "0";
-APT::Periodic::AutocleanInterval "0";
-APT::Periodic::Unattended-Upgrade "0";
+APT::Periodic::Update-Package-Lists \"0\";
+APT::Periodic::Download-Upgradeable-Packages \"0\";
+APT::Periodic::AutocleanInterval \"0\";
+APT::Periodic::Unattended-Upgrade \"0\";
 EOF
 "
 
