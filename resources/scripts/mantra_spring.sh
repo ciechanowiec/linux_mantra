13e2f046da1ba49331312a869b0f2eecd01f9c06
Herman Ciechanowiec <herman@ciechanowiec.eu>
2023-01-29 11:50:27 +0100

    Add logger

--- resources/scripts/mantra_spring.sh
+++ resources/scripts/mantra_spring.sh
@@ -119,9 +119,11 @@ insertContentToMain () {
 cat > "$mainFile" << EOF
 package $firstLevelPackageName.$secondLevelPackageName.$projectName;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class Main {
 
