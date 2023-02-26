8f5c9f7207a23b3a9e905c272b2fe18071c40a67
Herman Ciechanowiec <herman@ciechanowiec.eu>
2023-02-26 17:25:41 +0100

    Explain commands

--- resources/scripts/docker_clean_containers.sh
+++ resources/scripts/docker_clean_containers.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-allContainersIDs=$(sudo docker ps -aq)
+allContainersIDs=$(sudo docker ps --all --quiet)
 
 if [ ${#allContainersIDs} == 0 ]
 then
 	echo "[INFO]: There are no Docker containers to remove"
 else
 	echo "[INFO]: Removing Docker containers..."
-	sudo docker rm -vf $allContainersIDs # Do not surround this variable with quote marks because then it might work incorrectly
+	sudo docker rm --force --volumes $allContainersIDs # Do not surround this variable with quote marks because then it might work incorrectly
 fi
--- resources/scripts/docker_clean_images.sh
+++ resources/scripts/docker_clean_images.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-allImagesIDs=$(sudo docker images -aq)
+allImagesIDs=$(sudo docker images --all --quiet)
 
 if [ ${#allImagesIDs} == 0 ]
 then
 	echo "[INFO]: There are no Docker images to remove"
 else
 	echo "[INFO]: Removing Docker images..."
-	sudo docker rmi -f $allImagesIDs # Do not surround this variable with quote marks because then it might work incorrectly
+	sudo docker rmi --force $allImagesIDs # Do not surround this variable with quote marks because then it might work incorrectly
 fi
