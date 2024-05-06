8df11d564d891bedcfd22544e8540fcad3583722
Herman Ciechanowiec <herman@ciechanowiec.eu>
2024-05-06 20:25:57 +0200

    Update colima SSH

--- resources/scripts/colima_ssh.sh
+++ resources/scripts/colima_ssh.sh
@@ -3,6 +3,6 @@
 # Docs: https://stackoverflow.com/a/71265260
 
 tmpconfig=$(mktemp)
-(limactl show-ssh --format config colima | grep --invert-match "^  ControlPath\|  ^User"; echo "  ForwardAgent=yes") > "$tmpconfig"
-ssh -F "$tmpconfig" "$USER@lima-colima" "sudo mkdir -p /root/.ssh/; sudo cp ~/.ssh/authorized_keys /root/.ssh/authorized_keys"
-ssh -F "$tmpconfig" root@lima-colima
+(cat "$HOME/.colima/ssh_config" | grep --invert-match "^  ControlPath\|  ^User"; echo "  ForwardAgent=yes") > "$tmpconfig"
+ssh -F "$tmpconfig" "$USER@colima" "sudo mkdir -p /root/.ssh/; sudo cp ~/.ssh/authorized_keys /root/.ssh/authorized_keys"
+ssh -F "$tmpconfig" root@colima
