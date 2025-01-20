2ddcf8e1c697926001da2bc4c22d19c116d02ece
Herman Ciechanowiec <herman@ciechanowiec.eu>
2025-01-20 13:23:15 +0100

    Update GPG generation

--- resources/scripts/gpg-generate.sh
+++ resources/scripts/gpg-generate.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-echo "[INFO] Deleting all old keys"
-gpg --delete-secret-and-public-keys "$(gpg --list-keys --with-colons | awk -F: '/^pub/{print $5}')"
+echo "[INFO] Disabling IPv6 for dirmngr"
+DIRMNGR_CONF="$HOME/.gnupg/dirmngr.conf"
+if [ ! -f "$DIRMNGR_CONF" ]; then
+  mkdir -p "$(dirname "$DIRMNGR_CONF")"
+  touch "$DIRMNGR_CONF"
+fi
+if ! grep -qxF "disable-ipv6" "$DIRMNGR_CONF"; then
+  echo "disable-ipv6" >> "$DIRMNGR_CONF"
+fi
+gpgconf --kill dirmngr
+sleep 3
+
+echo "[INFO] Deleting all existing keys"
+EXISTING_KEYS=$(gpg --list-keys --with-colons | awk -F: '/^pub/ {print $5}')
+
+for KEY in $EXISTING_KEYS; do
+  gpg --yes --pinentry-mode loopback --delete-secret-and-public-keys "$KEY"
+done
 
 echo "[INFO] Generating a new key"
 gpg --gen-key
@@ -9,13 +25,16 @@ gpg --gen-key
 echo "[INFO] All existing keys:"
 gpg --list-keys
 
-echo "[INFO] Extracting the singular key"
-key_id=$(gpg --list-keys --with-colons | awk -F: '/^pub/{print $5}')
+echo "[INFO] Getting the newly generated key"
+NEW_KEY_ID=$(
+  gpg --list-keys --with-colons \
+    | awk -F: '/^pub/{last=$5} END{print last}'
+)
 
-echo "[INFO] The singular key ID: $key_id"
+echo "[INFO] The new key ID: $NEW_KEY_ID"
 
 echo "[INFO] Distributing the key to the server"
-gpg --keyserver keyserver.ubuntu.com --send-keys "$key_id"
+gpg --keyserver keyserver.ubuntu.com --send-keys "$NEW_KEY_ID"
 
 echo "[INFO] Retrieving the key from the server"
-gpg --keyserver keyserver.ubuntu.com --recv-keys "$key_id"
+gpg --keyserver keyserver.ubuntu.com --recv-keys "$NEW_KEY_ID"
