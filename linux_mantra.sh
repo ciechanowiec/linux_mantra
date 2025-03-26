d3a3cc1a4c716450dfc60ee30886005a3dab01b7
Herman Ciechanowiec <herman@ciechanowiec.eu>
2025-03-26 16:06:11 +0100

    Add mermaid

--- linux_mantra.sh
+++ linux_mantra.sh
@@ -305,6 +305,9 @@ echo "Installing TypeScript..."
 #   good: https://lindevs.com/install-typescript-on-ubuntu
 sudo npm install -g typescript # `npm` comes from node, so node must be preinstalled
 
+echo "Installing Mermaid CLI (diagramming tool)..."
+sudo npm install -g @mermaid-js/mermaid-cli
+
 informAboutProcedureEnd
 
 promptOnContinuation
